8548f2c47b923e971098f3d63fef9a55627b3833
Phodal Huang <h@phodal.com>
2025-01-21 11:38:00 +0800

    chore: udpate demos

--- demo.js
+++ demo.js
@@ -4,3 +4,44 @@ console.log(module)
 console.log(module.DiffClient.prototype)
 console.log(module.GitClient.prototype)
 console.log(module.MerkleClient.prototype)
+
+// class DiffClient {
+//     diff() {}
+//     diffLines() {}
+// }
+//
+// class GitClient {
+//     getCommitChain() {}
+//     getCommitVerifyData() {}
+//     getRepoHeadSha() {}
+//     getTotalCommitCount() {}
+//     getVerifyCommit() {}
+//     throwIfCommitDoesntExist() {}
+// }
+//
+// class MerkleClient {
+//     computeMerkleTree() {}
+//     computeMerkleTreeWithRipgrepIgnore() {}
+//     deleteFile() {}
+//     getAllDirFilesToEmbed() {}
+//     getAllFiles() {}
+//     getHashesForFiles() {}
+//     getImportantPaths() {}
+//     getNextFileToEmbed() {}
+//     getNumEmbeddableFiles() {}
+//     getSpline() {}
+//     getSubtreeHash() {}
+//     init() {}
+//     initWithRipgrepIgnore() {}
+//     isTooBig() {}
+//     updateFile() {}
+//     updateRootDirectory() {}
+// }
+
+console.log(module.DiffClient.prototype.constructor.toString())
+console.log(module.GitClient.prototype.constructor.toString())
+console.log(module.MerkleClient.prototype.constructor.toString())
+
+let diffClient = module.DiffClient();
+diffClient
+
