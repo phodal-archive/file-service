96fb970776d4a5b386e37560beaacd6b16a61133
Phodal Huang <h@phodal.com>
2025-01-21 11:29:05 +0800

    chore: setup repos

---
+++ demo.js
@@ -0,0 +1,6 @@
+var module = require("./index.js");
+
+console.log(module)
+console.log(module.DiffClient.prototype)
+console.log(module.GitClient.prototype)
+console.log(module.MerkleClient.prototype)
